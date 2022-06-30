4478993ac97d20b7104ae2a3254562b60f51229d
Ali Bahari <alibir80@gmail.com>
2022-06-30 17:13:56 +0430

    Add over-the-top collision detection, angle correction, new collision algorithm - some minor bugs in block corners

--- main.py
+++ main.py
@@ -24,9 +24,100 @@ class Ball():
             radius = self.radius
         )
 
+    def correctAngle(self):
+        if(self.angle):
+            while(self.angle > 2 * math.pi):
+                self.angle -= 2 * math.pi
+            
+            while(self.angle < 0):
+                self.angle += 2 * math.pi
+
+    def determineSideOfEntry(self, block):
+        if(
+            (
+                self.x >= block.x + MOVE_SPEED and
+                self.x <= block.x + block.width - MOVE_SPEED and
+                self.y >= block.y and
+                self.y <= block.y + MOVE_SPEED
+            ) or
+            (
+                self.y < self.x - block.x + block.y and
+                self.x < block.x + MOVE_SPEED and
+                self.y > block.y
+            ) or
+            (
+                self.y < -self.x + block.x + block.y + block.width and
+                self.x > block.x + block.width - MOVE_SPEED and
+                self.y > block.y
+            )
+        ):
+            return "Top"
+        
+        if(
+            (
+                self.x >= block.x and
+                self.x <= block.x + MOVE_SPEED and
+                self.y >= block.y + MOVE_SPEED and
+                self.y <= block.y + block.height - MOVE_SPEED
+            ) or
+            (
+                self.y > self.x - block.x + block.y and
+                self.x > block.x and
+                self.y < block.y + MOVE_SPEED
+            ) or
+            (
+                self.y < -self.x + block.x + block.y + block.height and
+                self.x > block.x and
+                self.y > block.y + block.height - MOVE_SPEED
+            )            
+        ):
+            return "Left"
+        
+        if(
+            (
+                self.x >= block.x + MOVE_SPEED and
+                self.x <= block.x + block.width - MOVE_SPEED and
+                self.y >= block.y + block.height - MOVE_SPEED and
+                self.y <= block.y + block.height
+            ) or
+            (
+                self.y > -self.x + block.x + block.y + block.height and
+                self.x < block.x + MOVE_SPEED and
+                self.y < block.y + block.height
+            ) or
+            (
+                self.y > self.x - block.x + block.y - block.width + block.height and
+                self.x > block.x + block.width - MOVE_SPEED and
+                self.y < block.y + block.height
+            )            
+        ):
+            return "Bottom"
+        
+        if(
+            (
+                self.x >= block.x + block.width - MOVE_SPEED and
+                self.x <= block.x + block.width and
+                self.y >= block.y + MOVE_SPEED and
+                self.y <= block.y + block.height - MOVE_SPEED
+            ) or 
+            (
+                self.y > -self.x + block.x + block.y + block.width and
+                self.x < block.x + block.width and
+                self.y < block.y + MOVE_SPEED
+            ) or
+            (
+                self.y < self.x - block.x + block.y - block.width + block.height and
+                self.x < block.x + block.width and
+                self.y > block.y + block.height - MOVE_SPEED
+            )             
+        ):
+            return "Right"
+
+        return "Inside"
+
     def detectCollision(self, block):
         blockNearestPointToBall_x = self.x; blockNearestPointToBall_y = self.y
-        collisionSide = ""; x_collision = False; y_collision = False 
+        collisionSide = "Inside"; x_collision = False; y_collision = False 
         
         if(self.x <= block.x):
             blockNearestPointToBall_x = block.x
@@ -43,12 +134,15 @@ class Ball():
             collisionSide = "Bottom"; y_collision = True
 
         if(math.sqrt( (blockNearestPointToBall_x - self.x) ** 2 + (blockNearestPointToBall_y - self.y) ** 2 ) > self.radius):
-            return None
+            return "NO_COLLISION"
         if(x_collision and y_collision):
             return "Corner"
+        if(collisionSide == "Inside"):
+            return self.determineSideOfEntry(block)
         return collisionSide
 
     def moveBall(self, isStartingBallMissing, ball_starting_point_x, blocks):
+        normalMoving = True
         self.isMoving = True
 
         if((self.x >= SCREEN_WIDTH or self.x <= 0) and (self.y <= 0)):
@@ -58,33 +152,46 @@ class Ball():
         elif(self.y <= 0):
             self.angle = 2 * math.pi - self.angle
         elif(self.y >= SCREEN_HEIGHT):
-            self.angle = -1
+            self.angle = None
         else:
             pass
 
-        for block in blocks:
-            collisionSide = self.detectCollision(block)
-            if(collisionSide):
-                if(collisionSide == "Right" or collisionSide == "Left"):
-                    self.angle = math.pi - self.angle
-                elif(collisionSide == "Bottom" or collisionSide == "Top"):
-                    self.angle = 2 * math.pi - self.angle
-                else:
-                    self.angle = math.pi + self.angle
-                
-                block.value -= 1
-
-        if(self.angle == -1):
+        if(self.angle is None):
             if(isStartingBallMissing):
                 self.y = 590; self.isMoving = False
                 isStartingBallMissing = False
                 return isStartingBallMissing, self.x
             
             self.x = ball_starting_point_x; self.y = 590; self.isMoving = False
-            return isStartingBallMissing, None
+            return isStartingBallMissing, None        
 
-        self.x += MOVE_SPEED * math.cos(self.angle); self.y -= MOVE_SPEED * math.sin(self.angle)
+        self.correctAngle()
+
+        for block in blocks:
+            hadCollision = False
+            while(True):
+                collisionSide = self.detectCollision(block)
+                if(collisionSide == "NO_COLLISION"):
+                    break
+
+                if(collisionSide == "Right" or collisionSide == "Left"):
+                    self.angle = math.pi - self.angle
+                elif(collisionSide == "Bottom" or collisionSide == "Top"):
+                    self.angle = 2 * math.pi - self.angle
+                else:
+                    self.angle = math.pi + self.angle
+
+                self.correctAngle()
+                self.x += MOVE_SPEED * math.cos(self.angle); self.y -= MOVE_SPEED * math.sin(self.angle)
+                hadCollision = True; normalMoving = False
+            
+            if(hadCollision):
+                block.value -= 1
+        
+        if(normalMoving):
+            self.x += MOVE_SPEED * math.cos(self.angle); self.y -= MOVE_SPEED * math.sin(self.angle)
         return isStartingBallMissing, None
+
 
 class Block():
     def __init__(self, x, y, value):
@@ -145,12 +252,17 @@ def degree2radian(degree):
 
 def generateNewRowOfBlocks(score):
     distanceToNextHorizontalBlock = 79    
-    generatedBlocks = []
+    generatedBlocks = []; idxs = set()
     numBlocks = randint(1, 10)
     for _ in range(numBlocks):
+        blockIdx = randint(0, 9)
+        while(blockIdx in idxs):
+            blockIdx = randint(0, 9)
+        idxs.add(blockIdx)
+        
         generatedBlocks.append(
             Block(
-                x = STARTING_BLOCK_X + randint(0, 9) * distanceToNextHorizontalBlock,
+                x = STARTING_BLOCK_X + blockIdx * distanceToNextHorizontalBlock,
                 y = STARTING_BLOCK_Y,
                 value = score
             )
@@ -182,9 +294,9 @@ textFont = pygame.font.SysFont("Comic Sans MS", 20)
 
 SCREEN_WIDTH = 800; SCREEN_HEIGHT = 600
 FPS = 30
-MOVE_SPEED = 5
+MOVE_SPEED = 6
 INDICATOR_LENGTH = 150
-STARTING_BLOCK_X = 10; STARTING_BLOCK_Y = 50
+STARTING_BLOCK_X = 7; STARTING_BLOCK_Y = 50
 
 screen = pygame.display.set_mode( [SCREEN_WIDTH, SCREEN_HEIGHT] )
 
